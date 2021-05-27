44029187cc08ed7e19039fa47aa8154aadbf930d
Guillaume Bedard <bgui34@gmail.com>
2021-05-27 14:46:27 -0400

    Created Event component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<div class="event">
+			<events></events>
+		</div>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Events from './components/Events.vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	name: 'App',
+	components: {
+		Events
+	}
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
 }
 </style>
---
+++ src/components/Events.vue
@@ -0,0 +1,24 @@
+<template>
+	<div>
+		<div class="card" v-for="event in events" :key="event.id">
+			<p>{{ event.title }}</p>
+		</div>
+	</div>
+</template>
+
+<script>
+import { events } from '../assets/data.js';
+
+export default {
+	data() {
+		return {
+			events: events
+		};
+	},
+	mounted() {
+		console.log(this.events);
+	}
+};
+</script>
+
+<style lang="scss" scoped></style>
